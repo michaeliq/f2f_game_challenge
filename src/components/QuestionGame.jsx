654dd2daf9e039b6285c81a07c296d68a9999470
Advance Servicios Integrales <122900399+ADVANCESAS@users.noreply.github.com>
2024-02-28 16:04:49 -0500

    fix

--- src/components/QuestionGame.jsx
+++ src/components/QuestionGame.jsx
@@ -1,10 +1,13 @@
+"use client"
+import { useAppSelector } from "@/redux/hooks"
 import "@/styles/components/QuestionGame.css"
 
 const QuestionGame = () => {
+    const game = useAppSelector(state => state.game)
     return (
         <div className="question-game">
             <img src={"/images/modulo_pregunta.png"} className="module-question" alt="Imagen de la pregunta" />
-            <p>Pregunta #1</p>
+            <p>Pregunta #{game.questionNumber}</p>
         </div>
     )
 }
